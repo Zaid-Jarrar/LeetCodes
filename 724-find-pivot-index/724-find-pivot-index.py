922829ed7c0fcf1df36a525d9c71f23d1a536740
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-07-27 00:23:49 +0300

    Time: 108 ms (98.97%), Space: 14.6 MB (41.92%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -8,10 +8,14 @@ class Solution(object):
 
         
         left_sum = 0 
-        if left_sum == sum(nums) - nums[0]:
+        sum = 0
+        for num in nums:
+            sum += num
+            
+        if left_sum == sum - nums[0]:
             return 0
         for index in range(len(nums)):
-            right_sum = sum(nums) - left_sum - nums[index]
+            right_sum = sum - left_sum - nums[index]
             
             if left_sum == right_sum:
                 return index
