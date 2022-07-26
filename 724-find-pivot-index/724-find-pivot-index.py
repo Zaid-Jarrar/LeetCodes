21264c33ad1bc5950b44bfc0588805fb02bdb10a
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-07-27 00:15:37 +0300

    Time: 1915 ms (26.24%), Space: 14.6 MB (21.64%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -6,13 +6,16 @@ class Solution(object):
         :rtype: int
         """
 
-
-        left_sum = 0   
+        
+        left_sum = 0 
+        if left_sum == sum(nums) - nums[0]:
+            return 0
         for index in range(len(nums)):
             right_sum = sum(nums) - left_sum - nums[index]
+            
             if left_sum == right_sum:
-
                 return index
+            
             left_sum = left_sum + nums[index]
 
 
