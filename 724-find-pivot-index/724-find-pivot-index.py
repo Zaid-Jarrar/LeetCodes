7083cbcf1abf11b78aa5340e9bec7e03f7aeffbe
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-07-26 23:55:31 +0300

    Time: 3874 ms (20.12%), Space: 14.4 MB (65.46%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -5,22 +5,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        def _walk(nums,index):
-            global result
 
-            if index < (len(nums)):
-                left = nums[:index]
 
-                left_sum=sum(left)
-                right_sum = sum(nums) - left_sum - nums[index]
-                
-                if left_sum == right_sum:
-                    result = index
-                    return result
-                index +=1
-                _walk(nums,index)
-            else:
-                result = -1
-        _walk(nums,0)
-        return result
+        left_sum = 0   
+        for index in range(len(nums)):
+        # if index < (len(nums)):
+            right_sum = sum(nums) - left_sum - nums[index]
+            if left_sum == right_sum:
 
+                return index
+            left_sum = left_sum + nums[index]
+
+
+        return -1
