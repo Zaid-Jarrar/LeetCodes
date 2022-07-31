48b9a1483221e3935811e64cda3239e1f53411ac
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-07-31 04:13:25 +0300

    Time: 22 ms (78.84%), Space: 13.5 MB (86.26%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,27 +1,34 @@
 class Solution(object):
     def isSubsequence(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # for index in range(len(t)):
-        new_t=list(t)
-        for t_char in t:
-            
-            # s_char = s[index]
-            # t_char = t[index]
         
-            if t_char not in s :
-                new_t.remove(t_char)
-            elif t_char in s and s.count(t_char) != new_t.count(t_char):         
-                new_t.remove(t_char)
+        s_counter = 0
+        t_counter=0
+        final = ''
+        if s == '':
+            return True
+        elif len(s) < len(t) and s != '' and t != '':
+
+            while s_counter < len(s) and t_counter < len(t):
                 
-        new_t = ''.join(new_t)
-        if new_t != s:
+                
+
+                if s[s_counter] == t[t_counter]:
+                    
+                    final += s[s_counter]
+                    s_counter +=1
+                    t_counter +=1
+
+
+                else:
+                    
+                    t_counter+=1
+            if final == s:
+              return True
             return False
-        return True
-                
+
+        return False
+ 
+
                 
         
                 
