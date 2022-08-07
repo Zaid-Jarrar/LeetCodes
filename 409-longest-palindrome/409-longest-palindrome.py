96f23e099719e7ff0fa78a072aafcd9f20713483
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-08-07 23:53:39 +0300

    Time: 22 ms (88.58%), Space: 13.4 MB (64.09%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -5,7 +5,10 @@ class Solution(object):
         :rtype: int
         
         """
-
+        # the main idea here is that we want to find the letters that only appeared once and place them in a dic or a set
+        # because then we can substract the length of the whole string by the length of the dic or set 
+        # we would only add 1 if the dic is not empty because logically only 1 single char of the dic can exist in the palindrome string
+        
         dic = {}
         length = 0
         for letter in s:    
