8be6247c84e4dd8dc4e7e59c85523b6d91ba6ba1
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-08-11 01:35:42 +0300

    Time: 45 ms (85.04%), Space: 13.8 MB (78.30%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,10 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        dic = {}
+        d = {}
+        for letter in s:
+            if letter not in dic:
+                dic[letter] = 1
+            else:
+                dic[letter] +=1
         
-        text_s = sorted(s)
-        text_t = sorted(t)
-        if text_s == text_t:
+        for letter in t:
+            d[letter] = d.get(letter,0) + 1
+        
+        if d == dic:
             return True
         return False
+            
+        # OR THIS WAY
+        
+        # text_s = sorted(s)
+        # text_t = sorted(t)
+        # if text_s == text_t:
+        #     return True
+        # return False
         
