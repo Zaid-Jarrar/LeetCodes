0ad42c9c8487379ace6a92fdcc4818985a5ae107
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-08-11 03:20:51 +0300

    Time: 238 ms (30.42%), Space: 14.5 MB (59.37%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -6,8 +6,19 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
+# the main idea is to check if the dic of s has the same number of letters as the dic of p but only if their length are             the same
+#collections.Counter() helps count the freq of letters in our strings and it is a dic
+# index is the sliding index of s starting from 0
+# add_index is the sliding index of s starting from the length of p
+# we compare between the dic_s and dic_p if they have the same letters and same count as well then we add the starting index to   the result list
+# instead of always slicing s towards a new 3 letters moving one index at a time
+# we substract  the count of the letter at the index location and if is now counts to 0 then we simply remove it thus   reducing the space
+# after that we add from the right side a new letter or we increase the count of an existing letter, depending on the case
+
+# we keep this pattern until the length of add_index is larger than the length of s
+# we return the result
         
-        
+    
         dic_s = collections.Counter(s[:len(p)])
         dic_p = collections.Counter(p)
         result = []
