73749816b673f268fd280db34b19d40ac28e2062
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-08-20 18:59:46 +0300

    Time: 32 ms (56.87%), Space: 13.6 MB (59.51%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -4,28 +4,17 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-
-        # i = 0
-        # prefix = strs[0]
-        # while i <= len(strs[0]):
-        #     if prefix == strs[-1][:len(prefix)]:
-        #         print(prefix)
-        #         return prefix
-        #     i +=1
-        #     prefix = strs[0][:len(prefix)-i]
+        
         i = 0
         strs.sort()
         word = strs[0]
         prefix = ''
         while i <= len(strs[0]) :
             prefix = word[:len(word)-i]
-            # print(i,prefix,strs[-1][:len(prefix)])
-            if prefix == strs[-1][:len(prefix)]:
-                # print('yes')
-          
+            if prefix == strs[-1][:len(prefix)]:          
                 return prefix
             i +=1
-            # return ''     
+     
             
             
         
