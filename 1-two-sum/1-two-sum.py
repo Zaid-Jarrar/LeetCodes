499eda55acccbe050f243b714c6d54bcba46770f
Zaid-Jarrar <97835706+Zaid-Jarrar@users.noreply.github.com>
2022-08-29 22:30:27 +0300

    Time: 84 ms (60.98%), Space: 14.1 MB (86.14%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -7,13 +7,23 @@ class Solution(object):
         """
         
         
-        result = []
+        dic = {}
         for index in range(len(nums)):
-            for index2 in range(len(nums)):
-                if index != index2:
-                    if nums[index] + nums[index2] == target:
-                        result.append(index)
-                        result.append(index2)
-                        return result
-        return result
+            num = nums[index]
+            difference = target - num
+            if difference not in dic:
+                dic[nums[index]] = index
+            else:
+                return [dic[difference],index]
+                
+        
+        # result = []
+        # for index in range(len(nums)):
+        #     for index2 in range(len(nums)):
+        #         if index != index2:
+        #             if nums[index] + nums[index2] == target:
+        #                 result.append(index)
+        #                 result.append(index2)
+        #                 return result
+        # return result
         
